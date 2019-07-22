934d1cc8970b3d360b42a66b90051dfbfd53d7d5
hye1n <hy21n@naver.com>
2019-07-22 16:48:06 +0900

    Solve I/O problem No.2442

---
+++ baekjoon/src/io/p2442.java
@@ -0,0 +1,30 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p2442 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine().trim());
+		for (int i = 0; i < n; i++) {
+			for (int j = (n - i); j > 1; j--) {
+				bw.append(" ");
+			}
+			for (int k = 0; k < 2 * (i + 1) - 1; k++) {
+				bw.append("*");
+			}
+			bw.append("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
