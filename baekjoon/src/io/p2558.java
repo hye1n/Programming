184f95cfec3c97a98314442da1c3e01ef532e283
hye1n <hy21n@naver.com>
2019-07-22 13:11:26 +0900

    Solve I/O problem No.2558

---
+++ baekjoon/src/io/p2558.java
@@ -0,0 +1,22 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p2558 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int a = Integer.parseInt(br.readLine().trim());
+		int b = Integer.parseInt(br.readLine().trim());
+		bw.append(String.valueOf(a + b));
+		bw.flush();
+		bw.close();
+	}
+
+}
