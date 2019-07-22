c11081949eb521deed6e3be6216ea0b477214890
hye1n <hy21n@naver.com>
2019-07-22 14:19:23 +0900

    Solve I/O problem No.10952

---
+++ baekjoon/src/io/p10952.java
@@ -0,0 +1,30 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class p10952 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		while (true) {
+			st = new StringTokenizer(br.readLine().trim());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (a == 0 && b == 0) {
+				break;
+			}
+			bw.append(String.valueOf(a + b) + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
