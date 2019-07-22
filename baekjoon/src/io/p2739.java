0df70f43cb6c5598f4a9b5876041426137577465
hye1n <hy21n@naver.com>
2019-07-22 15:39:46 +0900

    Solve I/O problem No.2739

---
+++ baekjoon/src/io/p2739.java
@@ -0,0 +1,23 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p2739 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine().trim());
+		for (int i = 1; i <= 9; i++) {
+			bw.append(n + " * " + i + " = " + (n * i) + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
