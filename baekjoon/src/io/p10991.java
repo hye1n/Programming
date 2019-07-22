a9eda424f1b000d68ebcff9b76bbff6b9b6fa695
hye1n <hy21n@naver.com>
2019-07-22 18:12:50 +0900

    Solve I/O problem No.10991

---
+++ baekjoon/src/io/p10991.java
@@ -0,0 +1,33 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p10991 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine().trim());
+		for (int i = 0; i < n; i++) {
+			for (int j = (n - i - 1); j > 0; j--) {
+				bw.append(" ");
+			}
+
+			for (int k = 0; k <= i; k++) {
+				String star = (k < i) ? "* " : "*";
+				bw.append(star);
+			}
+			bw.append("\n");
+		}
+		bw.flush();
+		bw.close();
+
+	}
+
+}
