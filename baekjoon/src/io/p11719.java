911800b9edd7b1aad1d8fce56dd3e470c44f2646
hye1n <hy21n@naver.com>
2019-07-22 14:48:12 +0900

    Solve I/O problem No.11719

---
+++ baekjoon/src/io/p11719.java
@@ -0,0 +1,23 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p11719 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String input;
+		while ((input = br.readLine()) != null) {
+			bw.append(input + "\n");
+			bw.flush();
+		}
+		bw.close();
+	}
+
+}
