54d239eaea006cbc65e75fdeb39b12229202d953
hye1n <hy21n@naver.com>
2019-07-22 16:01:06 +0900

    Solve I/O problem No.10818

---
+++ baekjoon/src/io/p10818.java
@@ -0,0 +1,30 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class p10818 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine().trim());
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			min = Math.min(num, min);
+			max = Math.max(num, max);
+		}
+		bw.append(min + " " + max);
+		bw.flush();
+		bw.close();
+	}
+
+}
