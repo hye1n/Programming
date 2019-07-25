b0b28073a149864f330d5212965a5ebcd6532482
hye1n <hy21n@naver.com>
2019-07-25 11:27:15 +0900

    Solve problem No.9095

---
+++ baekjoon/src/step12/p9095.java
@@ -0,0 +1,29 @@
+package step12;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p9095 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int tc = Integer.parseInt(br.readLine().trim());
+		int[] dp = new int[12];
+		dp[0] = 1;
+		dp[1] = 1;
+		dp[2] = 2;
+		for (int i = 3; i <= 11; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+		for (int i = 0; i < tc; i++) {
+			int num = Integer.parseInt(br.readLine().trim());
+			bw.append(dp[num] + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
