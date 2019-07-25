92ceef599af3b9d8ebabd8c504b523a15da29563
hye1n <hy21n@naver.com>
2019-07-25 14:26:03 +0900

    Solve again problem No.1904

--- baekjoon/src/step12/p1904.java
+++ baekjoon/src/step12/p1904.java
@@ -13,17 +13,14 @@ public class p1904 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine().trim());
-		int[] memo = new int[n + 1];
-		memo[0] = 0;// N이 0일때 0가지
-		memo[1] = 1;// N이 1일때 1가지
-		memo[2] = 2;// N이 2일때 2가지
-		for (int i = 3; i <= n; i++) {
-			memo[i] = memo[i - 1] + memo[i - 2];
-			memo[i] %= 15746;
+		int[] dp = new int[n + 1];
+		dp[0] = dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+			dp[i] %= 15746;
 		}
-		bw.append(String.valueOf(memo[n]));
+		bw.append(String.valueOf(dp[n]));
 		bw.flush();
 		bw.close();
-
 	}
 }
