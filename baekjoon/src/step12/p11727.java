773966616afde4fa84d1cf1de8f48b9209ceafc7
hye1n <hy21n@naver.com>
2019-07-25 11:15:07 +0900

    Solve again problem No.11727

--- baekjoon/src/step12/p11727.java
+++ baekjoon/src/step12/p11727.java
@@ -7,49 +7,22 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class p11727 {
-	static int[] dp;
 
-	// 2*n타일을 1*2, 2*1, 2*2 탕일로 채우는 방법의 수
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine().trim());
-		dp = new int[n + 1];
-		bw.append(String.valueOf(solve(n)));
-		bw.flush();
-		bw.close();
-	}
-
-	// top-down 방식
-	public static int solve(int n) {
-		if (n < 2) {
-			return 1;
-		} else if (n == 2) {
-			return 3;
-		} else {
-			if (dp[n] > 0) {
-				return dp[n];
-			}
-			dp[n] = solve(n - 1) + 2 * solve(n - 2);
-			dp[n] %= 10007;
-			return dp[n];
-		}
-	}
-
-	// bottom-up방식
-	public static int solve2(int n) {
-		if (n < 2) {
-			return n;
-		}
+		int[] dp = new int[n + 1];
 		dp[0] = dp[1] = 1;
-		dp[2] = 3;
-		for (int i = 3; i <= n; i++) {
+		for (int i = 2; i <= n; i++) {
 			dp[i] = dp[i - 1] + 2 * dp[i - 2];
 			dp[i] %= 10007;
 		}
-		return dp[n];
+		bw.append(String.valueOf(dp[n]));
+		bw.flush();
+		bw.close();
 	}
 
 }
