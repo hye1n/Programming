661c5f2b110b18beface4dc87af041f71af6d205
hye1n <hy21n@naver.com>
2019-07-25 10:53:51 +0900

    Solve problem No.1463

--- baekjoon/src/step12/p1463.java
+++ baekjoon/src/step12/p1463.java
@@ -16,24 +16,22 @@ public class p1463 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine().trim());
-		// dp[n] = n을 1로 만들때 연산을 사용하는 횟수의 최솟값
 		int[] dp = new int[n + 1];
-		dp[0] = 0;
-		dp[1] = 0;
+		dp[0] = dp[1] = 0;
 		for (int i = 2; i <= n; i++) {
-			dp[i] = dp[i - 1] + 1; // 1을 빼는 방법
+			dp[i] = dp[i - 1] + 1;// 1을 빼는 연산
 			if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+				// 3으로 나누어 떨어지면, 3으로 나누는 연산(최솟값이기에 if문에서 대소 비교 후 값 넣기)
 				dp[i] = dp[i / 3] + 1;
-
 			}
 			if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+				// 2로 나누어 떨어지면, 2로 나누는 연산(최솟값이기에 if문에서 대소 비교 후 값 넣기)
 				dp[i] = dp[i / 2] + 1;
 			}
 		}
 		bw.append(String.valueOf(dp[n]));
 		bw.flush();
 		bw.close();
-
 	}
 
 }
