cd12c1006ef0e5321732ca97cde25f77b4e3b5ab
hye1n <hy21n@naver.com>
2019-07-26 12:41:21 +0900

    Solve problem No.1149

---
+++ baekjoon/src/step12/p1149.java
@@ -0,0 +1,36 @@
+package step12;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p1149 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine().trim());
+		int[][] dp = new int[n][3];
+		for (int i = 0; i < n; i++) {
+			String input = br.readLine().trim();
+			String[] temp = input.split(" ");
+			for (int j = 0; j < 3; j++) {
+				dp[i][j] = Integer.parseInt(temp[j]);
+			}
+		}
+		for (int i = 1; i < n; i++) {
+			dp[i][0] += Math.min(dp[i - 1][1], dp[i - 1][2]);
+			dp[i][1] += Math.min(dp[i - 1][0], dp[i - 1][2]);
+			dp[i][2] += Math.min(dp[i - 1][0], dp[i - 1][1]);
+		}
+		int min = Math.min(Math.min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
+		bw.append(String.valueOf(min));
+		bw.flush();
+		bw.close();
+
+	}
+
+}
