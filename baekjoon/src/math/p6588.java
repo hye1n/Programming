433507f0c829b0157f551132ed853356ba918d7e
hye1n <hy21n@naver.com>
2019-07-29 18:17:56 +0900

    Modify Source Code problem No.6588

--- baekjoon/src/math/p6588.java
+++ baekjoon/src/math/p6588.java
@@ -7,48 +7,43 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class p6588 {
+	public static final int MAX = 1000000;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		boolean[] check = new boolean[MAX + 1];
+		check[0] = check[1] = true;
+		for (int i = 2; i * i <= MAX; i++) {
+			if (check[i] == true) {
+				continue;
+			}
+			for (int j = i + i; j <= MAX; j += i) {
+				check[j] = true;
+			}
+		}
 		while (true) {
 			int num = Integer.parseInt(br.readLine());
 			if (num == 0) {
 				break;
 			}
-			boolean ispossible = false;
+			boolean isPossible = false;
 			for (int i = 2; i < num; i++) {
-				if (isPrime(i) && isPrime(num - i)) {
+				if (!check[i] && !check[num - i]) {
 					bw.append(num + " = " + i + " + " + (num - i) + "\n");
-					ispossible = true;
+					isPossible = true;
 					break;
 				}
-				ispossible = false;
 			}
-			if (!ispossible) {
+			if (!isPossible) {
 				bw.append("Goldbach's conjecture is wrong.\n");
 			}
 		}
 		bw.flush();
 		bw.close();
 
-	}
-
-	public static boolean isPrime(int num) {
-		if (num == 1) {
-			return false;
-		}
-		int root = (int) Math.sqrt(num);
-		if (root == 1) {
-			return true;
-		}
-		for (int i = 2; i <= root; i++) {
-			if (num % i == 0) {
-				return false;
-			}
-		}
-		return true;
 	}
 
 }
