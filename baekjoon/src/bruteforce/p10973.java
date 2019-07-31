2b0de0f77d3a54eea4dbf7d8d1f5dbc5ffa7efd4
hye1n <hy21n@naver.com>
2019-07-31 18:15:42 +0900

    Solve problem No.10973

---
+++ baekjoon/src/bruteforce/p10973.java
@@ -0,0 +1,61 @@
+package bruteforce;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p10973 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine().trim());
+		String[] input = br.readLine().split(" ");
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(input[i]);
+		}
+		if (!prevPermutation(arr, n)) {
+			bw.append(String.valueOf(-1));
+		} else {
+			for (int i = 0; i < n; i++) {
+				bw.append(arr[i] + " ");
+			}
+		}
+		bw.flush();
+		bw.close();
+	}
+
+	public static boolean prevPermutation(int[] arr, int n) {
+		int i = n - 1;
+		while (i > 0 && arr[i] >= arr[i - 1]) {
+			i -= 1;
+		}
+		if (i <= 0) {
+			return false;
+		}
+		int j = n - 1;
+		while (arr[i - 1] <= arr[j]) {
+			j -= 1;
+		}
+		swap(arr, i - 1, j);
+		j = n - 1;
+		while (i < j) {
+			swap(arr, i, j);
+			i += 1;
+			j -= 1;
+		}
+		return true;
+	}
+
+	public static int[] swap(int[] arr, int i, int j) {
+		int temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+		return arr;
+	}
+
+}
