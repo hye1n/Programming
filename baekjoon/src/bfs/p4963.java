b341943b2fad5db53b7aa19fef1ebef7b9ae7da6
hye1n <hy21n@naver.com>
2019-08-16 17:51:02 +0900

    Solve again Problem No.4963

--- baekjoon/src/bfs/p4963.java
+++ baekjoon/src/bfs/p4963.java
@@ -10,47 +10,47 @@ import java.util.StringTokenizer;
 public class p4963 {
 	static int[][] map;
 	static boolean[][] visited;
-	static int[] dx = { -1, 1, 0, 0, -1, 1, 1, -1 };
-	static int[] dy = { 0, 0, -1, 1, 1, 1, -1, -1 };
-	static int w;
-	static int h;
+	static int[] dx = { -1, 1, 0, 0, 1, 1, -1, -1 };
+	static int[] dy = { 0, 0, -1, 1, 1, -1, -1, 1 };
+	static int w, h;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+	public static void main(String[] agrs) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		while (true) {
 			st = new StringTokenizer(br.readLine());
-			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
-			visited = new boolean[h][w];
-			map = new int[h][w];
-			if (w == 0 && h == 0) {
+			w = Integer.parseInt(st.nextToken());
+			if (h == 0 && w == 0) {
 				break;
 			}
-			for (int i = 0; i < h; i++) {
+			map = new int[w][h];
+			visited = new boolean[w][h];
+			for (int i = 0; i < w; i++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < w; j++) {
+				for (int j = 0; j < h; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			int cnt = 0;
-			for (int i = 0; i < h; i++) {
-				for (int j = 0; j < w; j++) {
-					if (map[i][j] == 1 && !visited[i][j]) {
-						bfs(i, j, ++cnt);
+			for (int i = 0; i < w; i++) {
+				for (int j = 0; j < h; j++) {
+					if (!visited[i][j] && map[i][j] == 1) {
+						bfs(i, j);
+						cnt += 1;
 					}
 				}
 			}
 			System.out.println(cnt);
 
 		}
+
 	}
 
-	public static void bfs(int x, int y, int cnt) {
+	public static void bfs(int x, int y) {
 		Queue<Pair> queue = new LinkedList<Pair>();
-		visited[x][y] = true;
 		queue.add(new Pair(x, y));
+		visited[x][y] = true;
 		while (!queue.isEmpty()) {
 			Pair p = queue.poll();
 			x = p.x;
@@ -58,10 +58,10 @@ public class p4963 {
 			for (int k = 0; k < 8; k++) {
 				int nx = x + dx[k];
 				int ny = y + dy[k];
-				if (nx >= 0 && ny >= 0 && nx < h && ny < w) {
+				if (nx >= 0 && ny >= 0 && nx < w && ny < h) {
 					if (map[nx][ny] == 1 && !visited[nx][ny]) {
-						visited[nx][ny] = true;
 						queue.add(new Pair(nx, ny));
+						visited[nx][ny] = true;
 					}
 				}
 			}
--- baekjoon/src/bfs/p4963_dfs.java
+++ baekjoon/src/bfs/p4963_dfs.java
@@ -8,62 +8,52 @@ import java.util.StringTokenizer;
 public class p4963_dfs {
 	static int[][] map;
 	static boolean[][] visited;
-	static int[] dx = { -1, 1, 0, 0, -1, 1, 1, -1 };
-	static int[] dy = { 0, 0, -1, 1, 1, 1, -1, -1 };
-	static int w;
-	static int h;
+	static int[] dx = { -1, 1, 0, 0, 1, 1, -1, -1 };
+	static int[] dy = { 0, 0, -1, 1, 1, -1, -1, 1 };
+	static int w, h;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+	public static void main(String[] agrs) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		while (true) {
 			st = new StringTokenizer(br.readLine());
-			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
-			visited = new boolean[h][w];
-			map = new int[h][w];
-			if (w == 0 && h == 0) {
+			w = Integer.parseInt(st.nextToken());
+			if (h == 0 && w == 0) {
 				break;
 			}
-			for (int i = 0; i < h; i++) {
+			map = new int[w][h];
+			visited = new boolean[w][h];
+			for (int i = 0; i < w; i++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < w; j++) {
+				for (int j = 0; j < h; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			int cnt = 0;
-			for (int i = 0; i < h; i++) {
-				for (int j = 0; j < w; j++) {
-					if (map[i][j] == 1 && !visited[i][j]) {
-						dfs(i, j, ++cnt);
+			for (int i = 0; i < w; i++) {
+				for (int j = 0; j < h; j++) {
+					if (!visited[i][j] && map[i][j] == 1) {
+						dfs(i, j);
+						cnt += 1;
 					}
 				}
 			}
 			System.out.println(cnt);
-
 		}
+
 	}
 
-	public static void dfs(int x, int y, int cnt) {
+	public static void dfs(int x, int y) {
 		visited[x][y] = true;
 		for (int k = 0; k < 8; k++) {
 			int nx = x + dx[k];
 			int ny = y + dy[k];
-			if (nx >= 0 && ny >= 0 && nx < h && ny < w) {
+			if (nx >= 0 && ny >= 0 && nx < w && ny < h) {
 				if (map[nx][ny] == 1 && !visited[nx][ny]) {
-					dfs(nx, ny, cnt);
+					dfs(nx, ny);
 				}
 			}
-		}
-	}
-
-	static class Pair {
-		int x, y;
-
-		Pair(int x, int y) {
-			this.x = x;
-			this.y = y;
 		}
 	}
 }
