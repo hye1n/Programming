7c231fccb68206f0a4730a523b5cf38e6f75361e
hye1n <hy21n@naver.com>
2019-08-18 16:48:51 +0900

    Solve again Problem No.1697

--- baekjoon/src/bfs/p1697.java
+++ baekjoon/src/bfs/p1697.java
@@ -3,44 +3,42 @@ package bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class p1697 {
 	static final int MAX = 100000;
-	static int[] time = new int[MAX + 1];
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] agrs) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
 		int n = Integer.parseInt(input[0]);
 		int k = Integer.parseInt(input[1]);
-		bfs(n, k);
-	}
-
-	public static void bfs(int n, int k) {
 		Queue<Integer> queue = new LinkedList<Integer>();
+		int[] dist = new int[MAX + 1];
+		Arrays.fill(dist, -1);
 		queue.add(n);
-		time[n] = 1;
+		dist[n] = 0;
+
 		while (!queue.isEmpty()) {
 			int x = queue.poll();
 			if (x == k) {
 				break;
 			}
-			if (x + 1 <= MAX && time[x + 1] == 0) {
-				queue.add(x + 1);
-				time[x + 1] = time[x] + 1;
-			}
-			if (x - 1 >= 0 && time[x - 1] == 0) {
+			if (x - 1 >= 0 && dist[x - 1] == -1) {
 				queue.add(x - 1);
-				time[x - 1] = time[x] + 1;
+				dist[x - 1] = dist[x] + 1;
 			}
-			if (x * 2 <= MAX && time[x * 2] == 0) {
+			if (x + 1 <= MAX && dist[x + 1] == -1) {
+				queue.add(x + 1);
+				dist[x + 1] = dist[x] + 1;
+			}
+			if (x * 2 <= MAX && dist[x * 2] == -1) {
 				queue.add(x * 2);
-				time[x * 2] = time[x] + 1;
+				dist[x * 2] = dist[x] + 1;
 			}
 		}
-		System.out.println(time[k] - 1);
-
+		System.out.println(dist[k]);
 	}
 }
